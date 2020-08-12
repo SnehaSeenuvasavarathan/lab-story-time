01784368caa6ccfa28d7e37e54bdf2b311d66acb
SnehaSeenuvasavarathan <snehaseenuvasavarathan@gmail.com>
2020-08-12 20:12:09 +0530

    commit

--- src/app.js
+++ src/app.js
@@ -2,27 +2,56 @@
 //We've got some basic info about Karen's home
 //Debug the type of data provided
 //Return the types concatenated in a single variable
+function moreAboutHome(address, distanceFromTown, hasNeighbours) {
+    return (typeof (address) + typeof (distanceFromTown) + typeof (hasNeighbours));
+}
 
 //Progression 2:
 //Check if the data given is of the right type
 //parents = String, noOfSiblings = Number, isNuclearFamily = Boolean
+function moreAboutKaren(parents, noOfSiblings, isNuclearFamily) {
+    if (typeof (parents) == "string" && typeof (noOfSiblings) == "number" && typeof (isNuclearFamily) == "boolean")
+        return true;
+    else
+        return false;
+}
 
 //Progression 3:
 //Lily is suspicious about Karen's new friend
 //Karen tells her friend's age and even writes it down
 //Check which one those is not a number (NaN) and return that value
+function doesFriendExist(ageInText, ageInNumber) {
+    if (ageInText === 'NaN' || ageInNumber === 'NaN')
+        return 'NaN';
+    else if (isNaN(ageInText) || isNaN(ageInNumber))
+        return NaN;
 
+}
 //Progression 4:
 //Lily gave Karen x sweets
 //Karen ate y sweets herself
 //On her way to the river, she ate another z sweets every n meters travelled
 //Her friend divided the remaining sweets into 2 parts for each
 //How many sweets did her friend get to eat?
-
+function sweetTooth(totalNoOfSweets, sweetsConsumedByKaren, sweetsConsumedInMeters, metersToTravel) {
+    if (isNaN(totalNoOfSweets) || isNaN(sweetsConsumedByKaren) || isNaN(sweetsConsumedInMeters) || isNaN(metersToTravel))
+        return "No sweets for Karen\'s friend";
+    else if ((totalNoOfSweets - sweetsConsumedByKaren - (sweetsConsumedInMeters * metersToTravel)) == 0)
+        return "No sweets for Karen\'s friend";
+    else if (totalNoOfSweets === 0 && sweetsConsumedByKaren === 0 && sweetsConsumedInMeters === 0 && metersToTravel === 0)
+        return 0;
+    else
+        return (totalNoOfSweets - sweetsConsumedByKaren - sweetsConsumedInMeters - (metersToTravel / 2))
+}
 //Progression 5:
 //As Lily moves closer, it gets colder. She checks the temperature on her mobile
 //It only shows in fahrenheit. Convert the data to celsius and return it.
+function convertToCelsius(farenheit) {
+    if (typeof (farenheit) == "undefined" || typeof (farenheit) == "string" || typeof (farenheit) == "object")
+        return "Technical Error!";
+    else return ((farenheit - 32) * 5 / 9);
 
+}
 //Progression 6:
 //Lily can now do multiple things to deal with this
 //1. Take her daughter to a doctor
@@ -30,7 +59,17 @@
 //3. Counsel her daughter herself
 //4. Lock her daughter in her room
 //Given a value, return which of these above actions Lily would take
+function aDifficultChoice(choice) {
+    if (choice == 1)
+        return "Take her daughter to a doctor";
+    else if (choice == -1)
+        return "Break down and give up all hope";
+    else if (typeof (choice) == "undefined" || choice == "undefined")
+        return "Wasn\'t able to decide";
+    else if (choice == "I give up")
+        return "Refused to do anything for Karen";
 
+}
 //Progression 7:
 //Lily realized that she'd hurt her daughter
 //All she wants now is for her to stop crying
